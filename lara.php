5c8a3d35338ee08019445ff2312d13f091076828
karanawate <karanawate8@gmail.com>
2021-12-12 20:21:16 +0530

    laravel suggestion package install name of laavel extra intelinses

--- lara.php
+++ lara.php
@@ -7,3 +7,10 @@ step 2: composer require maatwebsite/excel
 step 3 : create file : php artisan make:import ExelImport
 
 note: only supported .csv file 
+
+
+------------------------------------
+step 1 : control + shif + x  package install
+
+step 2 : Laravel Extra Intellisense
+
