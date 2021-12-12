8f9b8e630496111e9c1903c6d842e053cbb48efa
karanawate <karanawate8@gmail.com>
2021-12-12 20:19:13 +0530

    laravel Exel library use

---
+++ lara.php
@@ -0,0 +1,9 @@
+Documentation 
+Exel imported in laravel 
+step 1: skip extension: gd extension Enalbel remove ;
+
+step 2: composer require maatwebsite/excel
+
+step 3 : create file : php artisan make:import ExelImport
+
+note: only supported .csv file 
